ad514bfad11a45c914238c23d4ec443b18a4b875
Anik Saha <aniksahaearnbd@gmail.com>
2026-06-15 21:44:00 +0600

    Add Quiz Component
    Create a component that renders quiz items from an
    array of question and choices. Gets rendered
    after game is started.

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,45 @@ import blobTwo from "./assets/blob-two.png";
 
 function App() {
   const [isQuizStarted, setIsQuizStart] = useState(false);
+
+  const questionsArr = [
+    "What is My name?",
+    "What I am learning currently? ",
+    "What role I am seeking? ",
+    "Where do I live? ",
+    "What are you studying? ",
+  ];
+  const choices = [
+    ["Anik", "Usme", "Shoham", "Hajbi"],
+    ["React", "JavaScript", "Next.js", "Tailwind"],
+    [
+      "Front-End Engineer",
+      "Backend Engineer",
+      "Full-Stack Engineer",
+      "Software Engineer",
+    ],
+    ["Bangladesh", "USA", "Germany", "Japan"],
+    [
+      "Bsc in Engineering",
+      "Diploma in Enginering",
+      "Msc in Engineering",
+      "Phd",
+    ],
+  ];
+
   return (
     <main className="main">
       {!isQuizStarted && <IntroPage handleClick={() => setIsQuizStart(true)} />}
-      {isQuizStarted && <Quiz />}
+      {isQuizStarted && (
+        <>
+          {questionsArr.map((que, i) => (
+            <Quiz key={i} question={que} choicesArr={choices[i]} />
+          ))}
+        </>
+      )}
+      {isQuizStarted && (
+        <button className="start-quiz-btn check-btn">Check answers</button>
+      )}
       <img src={blobOne} alt="" className="blob-one" />
       <img src={blobTwo} alt="" className="blob-two" />
     </main>
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,5 +1,17 @@
-function Quiz() {
-  return <h1>Quiz Started</h1>;
+function Quiz({ question, choicesArr }) {
+  return (
+    <article className="quiz-container">
+      <h2 className="quiz-question">{question}</h2>
+      <div className="choices-container">
+        {choicesArr.map((choice, index) => (
+          <button key={index} className="quiz-choice">
+            {choice}
+          </button>
+        ))}
+      </div>
+      <div className="line"></div>
+    </article>
+  );
 }
 
 export default Quiz;
